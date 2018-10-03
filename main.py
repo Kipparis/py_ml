374112a01e2c3c8c5224c72f9a097e8d4e4ad4f5
Kipparis <keepintine@gmail.com>
2018-10-02 21:44:01 -0400

    Add requirements

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3 
 
+import pandas as pd
+# создаем простой набор данных с характеристиками пользователей
+data = {'Name': ["John", "Anna", "Peter", "Linda"],
+'Location' : ["New York", "Paris", "Berlin", "London"], 'Age' : [24, 13, 53, 33]
+}
+data_pandas = pd.DataFrame(data)
+# IPython.display позволяет "красиво напечатать" датафреймы 
+# в Jupyter notebook
+display(data_pandas)
